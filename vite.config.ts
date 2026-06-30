88ee438d5331ca56b1f4e2bde8dcb9f99c226411
Kauan Rodrigues <kauanrdx145@gmail.com>
2026-06-29 22:26:25 -0300

    site 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  tanstackStart: {
-    server: {
-      entry: "server",
-    },
-  },
+  // tanstackStart: {
+  //   server: {
+  //     entry: "server",
+  //   },
+  // },
 
   vite: {
     server: {
