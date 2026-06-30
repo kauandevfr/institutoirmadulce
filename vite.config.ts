e770608c3634475ebfab5852e06d51adc687590d
Kauan Rodrigues <kauanrdx145@gmail.com>
2026-06-29 23:45:02 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  tanstackStart: { server: { entry: "server" }, },
+  tanstackStart: { server: { entry: "server" } },
+  vite: {
+    server: { allowedHosts: ["institutoirmadulce.kauanrodrigues.com.br"] },
+    preview: { allowedHosts: ["institutoirmadulce.kauanrodrigues.com.br"] },
+  },
 });
