69216f7cc0f582daa41cc63b874780c5be311a22
Kauan Rodrigues <kauanrdx145@gmail.com>
2026-06-29 22:33:05 -0300

    voltei ao normal

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,5 @@
-import { defineConfig as defineLovableConfig } from "@lovable.dev/vite-tanstack-config";
-import { defineConfig, mergeConfig } from "vite";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-const lovableConfig = defineLovableConfig({
-  tanstackStart: { server: { entry: "server" } },
-});
-
-export default defineConfig(async (env) => {
-  const resolvedLovableConfig = await lovableConfig(env);
-
-  return mergeConfig(resolvedLovableConfig, {
-    server: {
-      host: "::",
-      port: 1007,
-    },
-    preview: {
-      host: "::",
-      port: 1007,
-      allowedHosts: [
-        ".kauanrodrigues.com.br",
-        "institutoirmadulce.kauanrodrigues.com.br",
-      ],
-    },
-  });
+export default defineConfig({
+  tanstackStart: { server: { entry: "server" }, },
 });
